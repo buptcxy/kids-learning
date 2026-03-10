fe3300b18b38928d9d71688f005dc52f46455b29
chenxiaoyu <buptcxy@163.com>
2026-03-10 17:18:39 +0800

    Add server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8081;
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml'
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'index.html');
+  
+  if (req.url !== '/') {
+    filePath = path.join(__dirname, req.url);
+  }
+  
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'text/plain';
+  
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end('Server Error: ' + err.code);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Kids Learning Server running at http://localhost:${PORT}/`);
+});
